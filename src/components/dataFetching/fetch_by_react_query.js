28381775e481b669863ab0fde68056dfd50586c2
DESKTOP-RLQEV9L\sufian <abusufian12@gmail.com>
2022-07-07 12:56:10 +0900

    add

--- src/components/dataFetching/fetch_by_react_query.js
+++ src/components/dataFetching/fetch_by_react_query.js
@@ -6,10 +6,12 @@ import getQuoteData from '../Api/getQuoteApi';
 export default function FetchDataByReactQuery({quote}){
     const {data} = useQuery("quote", () => getQuoteData());
 
+    // console.log(data);
+
     return(
         <div>
             <h2>This is React Query</h2>
-            <div>{data?.content}</div>
+            <div>{data.content}</div>
         </div>
     )
 }    
