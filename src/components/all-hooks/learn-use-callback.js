5d18168a3981803db9de56f90539110fef29eccb
DESKTOP-RLQEV9L\sufian <abusufian12@gmail.com>
2022-07-30 08:25:04 +0900

    add

--- src/components/all-hooks/learn-use-callback.js
+++ src/components/all-hooks/learn-use-callback.js
@@ -1,9 +1,28 @@
-import React from 'react';
-
+import React, { useState, useCallback} from 'react';
+import Todos from '../TodoList/Todos';
 export default function LearnCallback(){
+    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState([]);
+
+    const increment = () => {
+        setCount((c) => c + 1);
+    }
+
+    const addTodo = useCallback(() => {
+        setTodos((t) => [...t, 'New Todo']);
+    }, [todos]);
     return(
-        <div>
-            <h2>This is Callback Hooks</h2>
-        </div>
+        <>
+            <div>
+                <h2>This is Callback Hooks</h2>
+            </div>
+            <Todos todos={todos} addTodo={addTodo}/>
+            <hr />
+            <div>
+                Count: {count}
+                <button type='button' onClick={increment}>+</button>
+            </div>
+        </>
+        
     );
 }
